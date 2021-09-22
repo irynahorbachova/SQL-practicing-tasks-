52f7f5b9ae6c140d7de20100be33e4213d52f9d5
Iryna Horbachova <66974494+irynahorbachova@users.noreply.github.com>
2021-09-22 03:13:26 +0300

    Update and rename Adverts_Costs_Tables.sql to Query_Adverts_Costs_Tables.sql

--- Query_Adverts_Costs_Tables.sql
+++ Query_Adverts_Costs_Tables.sql
@@ -1,14 +1,3 @@
-CREATE TABLE adverts (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  category_name TEXT
-);
-
-CREATE TABLE costs (
-  id INTEGER PRIMARY KEY,
-  cost INTEGER
-);
-
 SELECT adverts.category_name, AVG(costs.cost) AS avg FROM costs
 LEFT JOIN adverts ON adverts.id = costs.id
 GROUP BY adverts.category_name
